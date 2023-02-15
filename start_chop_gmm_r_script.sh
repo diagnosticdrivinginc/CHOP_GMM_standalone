f251c326e2c7c1d12c62713e61967b9cb02b2e50
David Grethlein <12dgrethlein@gmail.com>
2023-02-15 22:57:42 +0800

    Passes command line arguments to R script via `docker run`

--- start_chop_gmm_r_script.sh
+++ start_chop_gmm_r_script.sh
@@ -2,5 +2,5 @@
 
 docker start chop_gmm_container
 docker exec chop_gmm_container git pull origin master
-docker exec chop_gmm_container Rscript chop_gmm_model.R
+docker exec chop_gmm_container Rscript chop_gmm_model.R $@
 docker stop chop_gmm_container
